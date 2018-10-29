a72fb8fd7f747aabf7106ad9d7cf29f6e63b2eaf
Alejandro Hernández Matías <alex@4andgo.com>
2018-10-29 04:22:10 +0100

    Added intent to filter status of character by season+episode

--- rickmorty/lambda/index.js
+++ rickmorty/lambda/index.js
@@ -14,19 +14,33 @@ const rickMortyCharactersApiUrl = 'https://rickandmortyapi.com/api/character/'
 const SKILL_NAME = 'Rick y Morty';
 const NUM_MAX_SEASONS = 3;
 
+const statusCharacterLive = "vivos"
+const statusCharacterLiveApi = "Alive"
+const statusCharacterDeath = "muertos"
+const statusCharacterDeathUrl = "Dead"
+const statusCharacterUnknown = "desaparecidos"
+const statusCharacterUnknownUrl = "unknown"
+
 const welcomeMessage = '¡Bienvenidos a Rick y Morty! Puedes preguntarme sobre el nombre de un episodio o los personajes que salen en él';
 const exitSkillMessage = 'Gracias por usar Rick y Morty para informarte de tu serie favorita!  Espero que vuelvas pronto!';
 const helpMessage = 'Conozco el estado de todos los personajes de la serie.  Prueba a preguntarme por un episodio y dime si quieres que te diga los personajes que están vivos, muertos o que se desconoce cómo se encuentran.  ¿Qué te episodio quieres consultar?';
 
-const errorSeasonMessage = 'Lo siento pero no he entendido el número de temporada, ¿Para cuál temporada quieres hacer el cálculo?';
+const errorSeasonMessage = 'La serie tiene 3 temporadas, ¿Para cuál temporada quieres hacer el cálculo?';
 const errorMaxSeasonMessage = 'La serie tiene 3 temporadas, por favor, dinos para cuál quieres hacer el cálculo';
 const errorSeasonReprontMessage = 'Para poder hacer el cálculo de número de episodios, tienes que decir el número de la temporada';
 
 const errorMaxEpisodeMessage = ', por favor dinos para cuál quieres hacer el cálculo';
 const errorEpisodeMessage = 'Lo siento pero no he entendido el número de episodio, ¿A qué número de episodio te refieres?';
 
+const errorMessageStatus = 'Los personajes pueden tener 3 estados, vivos, muertos y desaparecidos, dinos cual quieres seleccionar';
+const errorMessageStatusRe = 'Elige entre vivos, muertos o desaparecidos';
+
 const errorMessage = 'Lo siento no he podido entenderte, dime de nuevo qué es lo que quieres consultar';
-const errorMessageCharacters = 'Lo siento, pero no hemos podido recuperar la información sobre personajes que has solicitado';
+const errorMessageCharacters = 'Lo siento, pero no hemos podido recuperar personajes para la consulta que has realizado';
+
+/*getCharactersStatusSeasonAndNumber("muertos",2,2,(result) => {
+  console.log(result);
+});*/
 
 ////////////////////////////////
 // Regular handlers //
@@ -114,14 +128,15 @@ const GetNumEpisodesSeasonIntentHandler = {
       const responseBuilder = handlerInput.responseBuilder;
       const filledSlots = request.intent.slots;
       const slotValues = getSlotValues(filledSlots);
-      if(!slotValues.numSeason.resolved || slotValues.numSeason.resolved === "?" ||  slotValues.numSeason.resolved <= 0  || slotValues.numSeason.resolved > NUM_MAX_SEASONS){
+      
+      if(!slotValues.numSeason.resolved || slotValues.numSeason.resolved === "?" ||  parseInt(slotValues.numSeason.resolved) <= 0  || slotValues.numSeason.resolved > NUM_MAX_SEASONS){
           return responseBuilder
                               .speak(errorMaxSeasonMessage)
                               .reprompt(errorSeasonReprontMessage)
                               .addElicitSlotDirective('numSeason')
                               .getResponse();
       }
-      var numSeason = slotValues.numSeason.resolved;
+      var numSeason = parseInt(slotValues.numSeason.resolved);
       return responseBuilder.speak(getNumEpisodesSeason(numSeason)).getResponse();
   }
 };
@@ -136,28 +151,145 @@ const GetNameEpisodesSeasonIntentHandler = {
     const responseBuilder = handlerInput.responseBuilder;
     const filledSlots = request.intent.slots;
     const slotValues = getSlotValues(filledSlots);
-    if( slotValues.numSeason.resolved === "?"  || slotValues.numSeason.resolved <= 0  || slotValues.numSeason.resolved > NUM_MAX_SEASONS) {
+    if(!slotValues.numSeason.resolved || slotValues.numSeason.resolved === "?"  || parseInt(slotValues.numSeason.resolved) <= 0  || parseInt(slotValues.numSeason.resolved) > NUM_MAX_SEASONS) {
       return responseBuilder
                           .speak(errorSeasonMessage)
                           .reprompt(errorMaxSeasonMessage)
                           .addElicitSlotDirective('numSeason')
                           .getResponse();
-    }else if ( slotValues.numEpisode.resolved === "?" || slotValues.numEpisode.resolved > getNumEpisodesSeason(slotValues.numEpisode.resolved) ){
+    }else if (!slotValues.numEpisode.resolved || slotValues.numEpisode.resolved === "?" ||  parseInt(slotValues.numEpisode.resolved) <= 0  || parseInt(slotValues.numEpisode.resolved) > getNumEpisodesSeason(parseInt(slotValues.numSeason.resolved)) ){
       return responseBuilder
-                          .speak("La temporada "+slotValues.numEpisode.resolved+" tiene "+getNumEpisodesSeason(slotValues.numEpisode.resolved)+errorMaxEpisodeMessage)
+                          .speak(getNumEpisodesSeason(parseInt(slotValues.numSeason.resolved))+errorMaxEpisodeMessage)
                           .reprompt(errorEpisodeMessage)
                           .addElicitSlotDirective('numEpisode')
                           .getResponse();
     }
-    var numSeason = slotValues.numSeason.resolved;
-    var numEpisode = slotValues.numEpisode.resolved;
+    var numSeason = parseInt(slotValues.numSeason.resolved);
+    var numEpisode = parseInt(slotValues.numEpisode.resolved);
     return responseBuilder.speak(getNameEpisodeBySeasonAndNumber(numSeason,numEpisode)).getResponse();
   }
+};
+
+const InProgressGetStatusCharactersIntent = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+
+    return request.type === 'IntentRequest'
+      && request.intent.name === 'GetStatusCharactersIntent'
+      && request.dialogState !== 'COMPLETED';
+  },
+  handle(handlerInput) {
+      const responseBuilder = handlerInput.responseBuilder;
+      const currentIntent = handlerInput.requestEnvelope.request.intent;
+      const filledSlots = handlerInput.requestEnvelope.request.intent.slots;
+      const slotValues = getSlotValues(filledSlots);
+
+      if(!slotValues.numSeason.resolved || slotValues.numSeason.resolved === "?"  || parseInt(slotValues.numSeason.resolved) <= 0  || parseInt(slotValues.numSeason.resolved) > NUM_MAX_SEASONS) {
+        return responseBuilder
+                            .speak(errorSeasonMessage)
+                            .reprompt(errorMaxSeasonMessage)
+                            .addElicitSlotDirective('numSeason')
+                            .getResponse();
+      }else if (!slotValues.numEpisode.resolved || slotValues.numEpisode.resolved === "?" ||  parseInt(slotValues.numEpisode.resolved) <= 0  || parseInt(slotValues.numEpisode.resolved) > getNumEpisodesSeason(parseInt(slotValues.numSeason.resolved))){
+        return responseBuilder
+                            .speak(getNumEpisodesSeason(parseInt(slotValues.numSeason.resolved))+errorMaxEpisodeMessage)
+                            .reprompt(errorEpisodeMessage)
+                            .addElicitSlotDirective('numEpisode')
+                            .getResponse();
+      }else if(( slotValues.statusValue.isValidated == false || slotValues.statusValue.resolved === "?" ) && slotValues.statusValue.isAsked == true){
+          return handlerInput.responseBuilder
+                             .speak(errorMessageStatus)
+                             .reprompt(errorMessageStatusRe)
+                             .addElicitSlotDirective('statusValue')
+                             .getResponse()
+      }else{
+          return handlerInput.responseBuilder
+          .addDelegateDirective(currentIntent)
+          .getResponse();
+      }
+    },
+};
+
+const CompletedGetStatusCharactersIntentHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest' && request.intent.name === 'GetStatusCharactersIntent';
+  },
+  handle(handlerInput) {
+
+    const responseBuilder = handlerInput.responseBuilder;
+    const filledSlots = handlerInput.requestEnvelope.request.intent.slots;
+    const slotValues = getSlotValues(filledSlots);
+
+    var numSeason = parseInt(slotValues.numSeason.resolved);
+    var numEpisode = parseInt(slotValues.numEpisode.resolved);
+    var statusValue = slotValues.statusValue.resolved;
+
+    if (numSeason!== "?" && numEpisode!== "?" && statusValue!== "?"){
+      return new Promise((resolve) => {
+        getCharactersStatusSeasonAndNumber(statusValue,numSeason,numEpisode,(result) => {
+                  resolve(responseBuilder.speak(result).getResponse());
+              });
+          });
+    }else{
+      resolve(responseBuilder.speak(errorMessageCharacters).getResponse());
+    }
+  },
 };
 
 ////////////////////////////////
 // Episodes and Seasons functions//
 ////////////////////////////////
+
+function getCharactersStatusSeasonAndNumber(status,numSeason,numEpisode,callback) {
+    var realStatus = statusCharacterLiveApi
+    if (status == statusCharacterDeath){
+      realStatus = statusCharacterDeathUrl
+    }else if (status == statusCharacterUnknown){
+      realStatus = statusCharacterUnknownUrl
+    }
+    var currentCharactersUrl = getCharactersEpisodeBySeasonAndNumber(numSeason,numEpisode)
+    var urlParameters = ""
+    currentCharactersUrl.forEach(function(stringUrl){
+      var urlId = stringUrl.replace(rickMortyCharactersApiUrl,"")
+      if (urlParameters.length == 0){
+        urlParameters = urlId
+      }else{
+        urlParameters = urlParameters+","+urlId
+      }
+    });
+    if (urlParameters.length == 0){
+        callback(errorMessageCharacters);
+        return
+    }
+    var requestUrl = rickMortyCharactersApiUrl+urlParameters
+    request(requestUrl, function (error, response, body) {
+      if (error) {
+          callback(errorMessageCharacters);
+          return
+      } else {
+          var namesCharacters = "";
+          const currentCharacters = JSON.parse(body);    
+          currentCharacters.forEach(function(character){
+            if (character.status == realStatus){
+              if (namesCharacters.length == 0){
+                namesCharacters = character.name
+              }else{
+                namesCharacters = namesCharacters+", "+character.name
+              }
+            }
+          });
+          if (namesCharacters.length == 0){
+            callback(errorMessageCharacters);
+            return
+          }else{
+            callback("Estos son los personajes que he encontrado "+namesCharacters);
+            return
+          }
+      }
+  });
+}
+
 
 function getNumEpisodesSeason(numSeason){
   var currentEpisodes = getSeasonByNumber(numSeason);
@@ -167,9 +299,18 @@ function getNumEpisodesSeason(numSeason){
 function getNameEpisodeBySeasonAndNumber(numSeason,numEpisode){
   var currentEpisodes = getSeasonByNumber(numSeason);
   if (currentEpisodes.length >= numEpisode){
-    return "El título del episodio "+numEpisode+" de la temporada "+numSeason+" es "+ currentEpisodes[numEpisode].name;
+    return "El título del episodio "+numEpisode+" de la temporada "+numSeason+" es "+ currentEpisodes[numEpisode-1].name;
   }else{
     return "La temporada "+numSeason+" solo tiene "+ currentEpisodes.length + " episodios";
+  }
+}
+
+function getCharactersEpisodeBySeasonAndNumber(numSeason,numEpisode){
+  var currentEpisodes = getSeasonByNumber(numSeason);
+  if (currentEpisodes.length >= numEpisode){
+    return currentEpisodes[numEpisode-1].characters;
+  }else{
+    return [];
   }
 }
 
@@ -178,8 +319,10 @@ function getSeasonByNumber(numSeason){
   switch(numSeason) {
     case 2:
         currentEpisodes = episodes.EPISODES_SEASON_2;
+        break;
     case 3:
         currentEpisodes = episodes.EPISODES_SEASON_3;
+        break;
     default:
         break;
   }
@@ -244,6 +387,8 @@ exports.handler = Alexa.SkillBuilders.custom()
     .addErrorHandlers(ErrorHandler)
     .addRequestHandlers(LaunchRequestHandler,
                          HelpIntentHandler,
+                         InProgressGetStatusCharactersIntent,
+                         CompletedGetStatusCharactersIntentHandler,
                          GetNumEpisodesSeasonIntentHandler,
                          GetNameEpisodesSeasonIntentHandler,
                          CancelAndStopIntentHandler,
